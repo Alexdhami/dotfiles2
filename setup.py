16ac7c512c95652c294b064d9d6d5a84a3dddc54
alexdhami <b2ok369@proton.me>
2025-12-26 00:45:00 +0545

    removed unnecessary files

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ def main():
     neededPackages = ["wl-copy","pavucontrol","cava","swww","rofi","zsh","swaync","waybar","nwg-theme","xdg-desktop-portal-gtk","xdg-portal-hyprland","adw-gtk-dark","qt5ct","qt6ct","grim","slurp","kitty","thunar","wpctl","brightnessctl","network-manager-applet","zoxide","exa","gammastep","tealdeer","noto-fonts-emoji","ttf-jetbrains-mono-nerd","libcanberra","camera-shutter"]
     installNeededPackages(neededPackages)
 
-    dotfilesIncludedFiles = [".zshrc",".pk10k.zsh",".oh-my-zsh",".config/cava",".config/gtk-3.0",".config/gtk-4.0",".config/hypr",".config/kitty",".config/nwg-look",".config/qt5ct",".config/qt6ct",".config/rofi",".config/swaync",".config/waybar",".config/xdg-desktop-portal"]
+    dotfilesIncludedFiles = [".zshrc",".oh-my-zsh",".config/gtk-3.0",".config/gtk-4.0",".config/hypr",".config/kitty",".config/nwg-look",".config/qt5ct",".config/qt6ct",".config/rofi",".config/swaync",".config/waybar",".config/xdg-desktop-portal"]
     backupFileFolders(home_dir,dotfilesIncludedFiles)
     createSymlink(home_dir,dotfilesIncludedFiles)
     setUpNormalStuff()
