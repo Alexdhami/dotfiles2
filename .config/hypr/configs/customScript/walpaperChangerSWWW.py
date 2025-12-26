a387cf1034442857ff98331c6f072d573256eac8
alexdhami <b2ok369@proton.me>
2025-12-26 17:20:10 +0545

    structured the script little more

--- .config/hypr/configs/customScript/walpaperChangerSWWW.py
+++ .config/hypr/configs/customScript/walpaperChangerSWWW.py
@@ -3,27 +3,40 @@
 import random
 import os
 import subprocess
-import sys
 
-home_dir = os.path.expanduser("~")
-parentDir = f"{home_dir}/Pictures/Wallpapers/"
+    
+def changeWallpaper(WallpapersDir:str):
 
-try:
+    if not os.path.exists(WallpapersDir):
+        subprocess.run(["notify-send",f"No path -> {WallpapersDir}"])
+        return
+     
+    imageList = [img for img in os.listdir(WallpapersDir) if img.lower().endswith((".png",".jpg",".jpeg"))]
 
-    imagesList = [img for img in os.listdir(parentDir) if img.lower().endswith((".png",".jpg",".jpeg"))]
 
-except:
-    sys.exit(1)
+    if not imageList:
+        subprocess.run(["notify-send","no image found."])
+        return
 
-if not imagesList:
-    sys.exit(1)
+    randomImg = random.choice(imageList)
+    imgName = randomImg.split('.')[0]
+    fullImgpath = os.path.join(WallpapersDir, randomImg)
 
-# List of transitions
-transitions = ["fade","wipe","grow","center","outer","any"]
-randomImageDir =  parentDir + random.choice(os.listdir(parentDir))
+    transitions = ["fade","wipe","grow","center","outer","any"]
+    randomTransition = random.choice(transitions)
+    subprocess.run(["swww", "img","--transition-type",randomTransition,fullImgpath ])
+    return imgName
 
-try:
-    subprocess.run(["swww", "img","--transition-type",random.choice(transitions),randomImageDir ])
-except:
-    sys.exit(1)
+def sendNotification(imgName) -> None:
+    if imgName:
+        subprocess.run(["notify-send",f"Name: {imgName}"])
+
+def main():
+    home_dir = os.path.expanduser("~")
+    WallpapersDir = os.path.join(home_dir,"Pictures","Wallpapers")
+    imgName = changeWallpaper(WallpapersDir)
+    sendNotification(imgName)
+
+if __name__=="__main__":
+    main()
 
