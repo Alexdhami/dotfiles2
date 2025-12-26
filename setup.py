4b8caef8a8ade80db240d68de38be33b0ef8a178
Alexdhami <124015730+Alexdhami@users.noreply.github.com>
2025-12-26 12:36:36 +0545

    orderred all the functions

--- setup.py
+++ setup.py
@@ -142,10 +142,10 @@ def main():
     neededPackages = ["wl-copy","pavucontrol","cava","swww","rofi","zsh","swaync","waybar","nwg-theme","xdg-desktop-portal-gtk","xdg-portal-hyprland","adw-gtk-dark","qt5ct","qt6ct","grim","slurp","kitty","thunar","wpctl","brightnessctl","network-manager-applet","zoxide","exa","gammastep","tealdeer","noto-fonts-emoji","ttf-jetbrains-mono-nerd","libcanberra","camera-shutter"]
 
     dotfilesIncludedFiles = [".zshrc",".config/gtk-3.0",".config/gtk-4.0",".config/hypr",".config/kitty",".config/nwg-look",".config/qt5ct",".config/qt6ct",".config/rofi",".config/swaync",".config/waybar",".config/xdg-desktop-portal"]
-    # installNeededPackages(neededPackages)
-    # backupFileFolders(home_dir,dotfilesIncludedFiles)
-    # createSymlink(home_dir,dotfilesIncludedFiles)
     setUpNormalStuff(home_dir)
+    backupFileFolders(home_dir,dotfilesIncludedFiles)
+    createSymlink(home_dir,dotfilesIncludedFiles)
+    installNeededPackages(neededPackages)
 
 
 if __name__=="__main__":
