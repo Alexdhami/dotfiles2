1965c3a9442ac04a4d67974b489ac6a6936fdb23
alexdhami <b2ok369@proton.me>
2026-02-04 17:33:37 +0545

    Changed api site from api.dictionaryapi.dev -> https://freedictionaryapi.com/ and modified the script according to the json it returned also added an example send of that word on notify-send

--- .config/hypr/configs/customScript/search_word_meaning.py
+++ .config/hypr/configs/customScript/search_word_meaning.py
@@ -7,30 +7,45 @@ def selected_text():
     return subprocess.check_output(["wl-paste","--primary", "--no-newline"]).decode()
 
 def word_meaning(json_format):
-    if isinstance(json_format,tuple):
-        return "Something went wrong, maybe internet..."
+    if json_format is None:
+        return "Something went wrong, maybe bad internet ...😭"
 
     elif isinstance(json_format,dict):
-        return "No word Found."
+        if not json_format['entries']:
+            return "No meaning found of that word.🥺"
+        else:
+            return json_format['entries'][0]['senses'][0]['definition']
 
-    elif isinstance(json_format,list):
-        return json_format[0]['meanings'][0]['definitions'][0]['definition']
+def get_example(json_format):
+    if json_format is None:
+        return "Can't get examples if you get errors.🥴"
+
+    entries = json_format['entries']
+
+    for entry in entries:
+        for sense in entry['senses']:
+            examples = sense.get('examples', [])
+            if examples:  
+                return examples[0]
+
+    return "No example found.🫩"
 
 def fetch_word_in_site(site):
     try:
         return requests.get(site).json()
 
     except requests.exceptions.RequestException as e:
-        return ()
+        return None
 
-def send_notification(word, output):
-    subprocess.run(["notify-send","-t", "15000",f"Word: {word}",f"meaning -> {output}"])
+def send_notification(word, meaning,json_format):
+    subprocess.run(["notify-send","-t", "15000",f"Word: {word}",f"\nmeaning -> {meaning} \n\n example -> {get_example(json_format)}"])
 
 def main():
     word = selected_text()
-    site = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+    site = f"https://freedictionaryapi.com/api/v1/entries/en/{word}"
     json_format = fetch_word_in_site(site)
-    send_notification(word,word_meaning(json_format))
+    example = get_example(json_format)
+    send_notification(word,word_meaning(json_format),json_format)
 
 if __name__=="__main__":
     main()
