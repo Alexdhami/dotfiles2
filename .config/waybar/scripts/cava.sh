5ede0776da4cba3cbeaa241d2d69aa524ac5eb11
alexdhami <b2ok369@proton.me>
2026-02-10 00:15:05 +0545

    make script more flexible with checking if audio daemon starts

--- .config/waybar/scripts/cava.sh
+++ .config/waybar/scripts/cava.sh
@@ -9,7 +9,24 @@ for ((i=0; i<${#bar}; i++)); do
 done
 
 # Write cava config
-config_file="polybar_cava_config"
+config_file="/tmp/polybar_cava_config"
+
+touch $config_file
+
+echo "[general]
+bars = 18
+framerate = 60
+
+[output]
+method = raw
+raw_target = /dev/stdout
+data_format = ascii
+ascii_max_range = 7" >> $config_file
+
+# wait for paulseaudio/pipewire starts
+until pactl info >/dev/null 2>&1; do
+    sleep 0.2
+done
 
 # Use sed once in the pipeline instead of per-line
 cava -p "$config_file" | sed -u "$dict"
