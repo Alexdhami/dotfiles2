ac081e41b2fde480906f4fe90176ac6c7b0faaeb
alexdhami <b2ok369@proton.me>
2026-03-06 23:54:18 +0545

    Add some comments and make script more dynamic

--- .config/hypr/configs/customScript/walpaperChangerSWWW.py
+++ .config/hypr/configs/customScript/walpaperChangerSWWW.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python3
 
+# Requires to have folder structures like  /home/user/Pictures/Wallpapers/<folder_name>/image.png
+
 import random
 import os
 import subprocess
 
-    
-def changeWallpaper(WallpapersDir:str):
+def getRandomWallpaperDirName(WallpapersDir:str) :
+    if (len(os.listdir(WallpapersDir)) == 0):
+        return None
+    return os.path.join(WallpapersDir,random.choice(os.listdir(WallpapersDir)))
 
+def changeWallpaper(WallpapersDir:str):
     if not os.path.exists(WallpapersDir):
         subprocess.run(["notify-send",f"No path -> {WallpapersDir}"])
         return
-     
-    imageList = [img for img in os.listdir(WallpapersDir) if img.lower().endswith((".png",".jpg",".jpeg"))]
 
+    random_wallpaper =  getRandomWallpaperDirName(WallpapersDir)
+    if random_wallpaper is None:
+        subprocess.run(["notify-send","No wallpapers Directory inside ",f"{WallpapersDir}/.."])
+        return
 
+    imageList = [os.path.join(random_wallpaper,img) for img in os.listdir(random_wallpaper) if img.lower().endswith((".png",".jpg",".jpeg",".webp"))]
     if not imageList:
         subprocess.run(["notify-send","no image found."])
         return
 
     randomImg = random.choice(imageList)
-    imgName = randomImg.split('.')[0]
-    fullImgpath = os.path.join(WallpapersDir, randomImg)
-
     transitions = ["fade","wipe","grow","center","outer","any"]
     randomTransition = random.choice(transitions)
-    subprocess.run(["swww", "img","--transition-type",randomTransition,fullImgpath ])
-    return imgName
+    subprocess.run(["swww", "img","--transition-type",randomTransition,randomImg ])
+    return os.path.join(random_wallpaper, randomImg)
 
-def sendNotification(imgName) -> None:
-    if imgName:
-        subprocess.run(["notify-send",f"Name: {imgName}"])
+def sendImageChangedNotification(imgNamePath) -> None:
+    if imgNamePath:
+        subprocess.run(["notify-send","Wallpaper changed to ",f"{imgNamePath}"])
 
 def main():
     home_dir = os.path.expanduser("~")
     WallpapersDir = os.path.join(home_dir,"Pictures","Wallpapers")
-    imgName = changeWallpaper(WallpapersDir)
-    sendNotification(imgName)
+    sendImageChangedNotification(changeWallpaper(WallpapersDir))
 
 if __name__=="__main__":
     main()
-
